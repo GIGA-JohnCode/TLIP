4b298281962995d21f477f86a62afd459dc3cb66
GIGA-JohnCode <16.tasnimulhasan@gmail.com>
2025-04-24 12:30:43 +0000

    Thu, Apr 24, 2025, 6:30 PM +06:00

--- include/tlip.h
+++ include/tlip.h
@@ -5,5 +5,11 @@
 #include "types.h"
 #include "util.h"
 
+#include <gtk/gtk.h>
+#include <stdbool.h>
+
+extern bool cli_mode;
+extern 
+
 int cli_main(void);
 int gui_main(void);
--- include/util.h
+++ include/util.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int get_int(const char *prompt, int min_value, int max_value);
+void show_error(const char *error_message);
